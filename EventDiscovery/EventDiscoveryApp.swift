3c67796646d32d93b45348dc448cec55753445e8
yigit <yigitozdamar@hotmail.com>
2023-04-08 16:46:38 +0300

    Initial Commit

---
+++ EventDiscovery/EventDiscoveryApp.swift
@@ -0,0 +1,17 @@
+//
+//  EventDiscoveryApp.swift
+//  EventDiscovery
+//
+//  Created by Yigit Ozdamar on 8.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct EventDiscoveryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
